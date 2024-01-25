dbd9d42fde6d8317283d787eb0417ea224801f8c
AIA-P <adeyemiibukun05@gmail.com>
2024-01-25 20:50:14 +0100

    C

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - This function returns the sun of a variable
+ * number of variable passed as arguements.
+ *
+ * @n: The number of arguments to be passed.
+ *
+ * Return: sum of all args.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
